49a0d62f0d8cf6b98db6d21cee060a2b2dfec88a
Ragex <inbox.sis@gmail.com>
2017-05-17 16:30:23 +0300

    Add drawTable() func

--- script.js
+++ script.js
@@ -15,20 +15,20 @@ function readLocalStorage()
 	var strData = JSON.parse(localStorage.getItem('WTDL')); 	
 
 	if(strData){
-		var obj ={};
+		// var obj ={};
 
-		for(var i=0; i<strData.length;i++){
-			obj = JSON.parse(strData[i]);				
-			obj.updated = new Date(obj.updated);
+		// for(var i=0; i<strData.length;i++){
+		// 	obj = JSON.parse(strData[i]);				
+		// 	obj.updated = new Date(obj.updated);
 			
-			$('#table-header').after("<tr><td><input type='checkbox' " + chkboxState(obj.done) + "></input></td>\
-									 <td>"+obj.title+"</td><td>"+obj.author+"</td>\
-									 <td>"+check10(obj.updated.getDate())+"."
-									 +check10(obj.updated.getMonth()+1)+"."
-									 +obj.updated.getFullYear()
-									 +" "+check10(obj.updated.getHours())+":"
-									 +check10(obj.updated.getMinutes())+"</td></tr>");
-		}
+		// 	$('#table-header').after("<tr><td><input type='checkbox' " + chkboxState(obj.done) + "></input></td>\
+		// 							 <td>"+obj.title+"</td><td>"+obj.author+"</td>\
+		// 							 <td>"+check10(obj.updated.getDate())+"."
+		// 							 +check10(obj.updated.getMonth()+1)+"."
+		// 							 +obj.updated.getFullYear()
+		// 							 +" "+check10(obj.updated.getHours())+":"
+		// 							 +check10(obj.updated.getMinutes())+"</td></tr>");
+		// }
 			
 			// $('#tskTable'+' td:nth-child(2)').attr('contenteditable','true');
 			// $('#tskTable'+' td:nth-child(3)').attr('contenteditable','true');
@@ -52,6 +52,8 @@ function addTask(storage)
 
 	storage.push(JSON.stringify(taskItem));
 	localStorage.setItem('WTDL',JSON.stringify(storage));
+
+	return taskItem;
 }
 
 
@@ -72,36 +74,49 @@ function validateInputFields()
 }
 
 
+function drawTable(tasks)
+{
+	var obj ={};
+
+	if(tasks.length){
+  		for(var i=0; i<tasks.length;i++){
+			obj = JSON.parse(tasks[i]);				
+			obj.updated = new Date(obj.updated);
+			
+			$('#table-header').after("<tr><td><input type='checkbox' " + chkboxState(obj.done) + "></input></td>\
+									 <td>"+obj.title+"</td><td>"+obj.author+"</td>\
+									 <td>"+check10(obj.updated.getDate())+"."
+									 +check10(obj.updated.getMonth()+1)+"."
+									 +obj.updated.getFullYear()
+									 +" "+check10(obj.updated.getHours())+":"
+									 +check10(obj.updated.getMinutes())+"</td></tr>");
+		}
+	}
+	else{ //if we have only one task
+		 $('#table-header').after("<tr><td><input type='checkbox' " + chkboxState(tasks.done) + "></input></td>\
+									<td>"+tasks.title+"</td><td>"+tasks.author+"</td>\
+									<td>"+check10(tasks.updated.getDate())+"."
+									+check10(tasks.updated.getMonth()+1)+"."
+									+tasks.updated.getFullYear()
+									+" "+check10(tasks.updated.getHours())+":"
+									+check10(tasks.updated.getMinutes())+"</td></tr>");
+	}
+}
 
 
 
 $(document).ready(
 	function(){
 		var taskList = readLocalStorage();
+		drawTable(taskList);
 
 		console.log(taskList);
 		
 		$('#btnAddTask').click(
 			function(){
-
-					// if(logItem.title == ""){
-					//    $('#tsk_title').addClass("set-warning");
-					// }
-					// else if(logItem.author == ""){
-					// 	        $('#tsk_title').removeClass("set-warning");
-					// 			$('#tsk_auth').addClass("set-warning");
-					// }
-					// else{
-					// 	$('#tsk_title').removeClass("set-warning");
-					// 	$('#tsk_auth').removeClass("set-warning");
-					// 	$('#tsk_title').val("");$('#tsk_auth').val("");
-						
 						if(validateInputFields()){
-							addTask(taskList);	
+							drawTable(addTask(taskList));	
 							$('#tsk_title').val(""); $('#tsk_auth').val("");
 						}
-
-					//}	
-			});
-		
-	});
+		});	
+});
